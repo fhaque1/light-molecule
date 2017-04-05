69e1b6b2a8245a21360e1977240bc6c11a0001d6
max Bertfield <mbertfield@stuy.edu>
2017-04-05 01:35:14 -0400

    finished all texts

--- static/js.js
+++ static/js.js
@@ -6,14 +6,16 @@ var colorSwitch = function(i,j, currentR, currentG, currentB){
 //    return "fill:rgb(" + newR + "," + newG + "," + newB + "); stroke-width:1; stroke:#808080;";
     return "rgb(" + newR + "," + newG + "," + newB + ")";
 };
+var names = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+
+var text = d3.select("[id='america']").select("g").selectAll("text").data(names).enter().append("text");
+var texters = d3.selectAll("text");
+
 var updateMap = function(e){
     var lists = JSON.parse(document.getElementById("dObj").innerHTML);
     var data = lists[(document.getElementsByClassName("ui-slider-handle")[0].getAttribute("aria-valuenow") - 1977)];
     console.log(data);
-    var names = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
     var squares = d3.selectAll("rect");
-
-    
     squares.data(data);
     console.log(squares);
     squares.attr("style", "").attr("stroke","#808080");
@@ -25,9 +27,7 @@ var updateMap = function(e){
 	}
     });
 
-    var text = d3.select("[id='america']").select("g").selectAll("text").data(names).enter().append("text");
-    var text = d3.selectAll("text");
-    text.transition().duration(2000)
+    texters.transition().duration(2000)
 	.attr("x", function(d,i) { return squares[0][i].x.animVal.value + (50 * (1 + (data[i] /20) ) /2 );})
 	.attr("y", function(d,i) { return squares[0][i].y.animVal.value + ( 50 * (1 + (data[i] /20) )/2 );})
 	.attr("font-family", "sans-serif")
@@ -37,9 +37,29 @@ var updateMap = function(e){
 	.text( function(d){return d;});
     console.log(text);
 
+    var legenddata = [-8,-6,-4,-2,0,2,4,6,8,10];
+    var legendnames = ["-8+%","-6%","-4%","-2%","0%","2%","4%","6%","8%","10+%"]
+    var legs = d3.selectAll("line").data(legenddata).attr("stroke", function(d) {
+	if(d < 0){
+	    return colorSwitch(-1 * d, 10, 255,0,0);
+	}else{
+	    return colorSwitch(d,10,0,255,0);
+	}
+    });
+    var legstxt = d3.select("[id='america']").select("g").selectAll("text[class=legend]").data(legendnames).enter().append('text').attr('class','legend');
+    var legst = d3.selectAll("text[class=legend]")
+	.attr("x", function(d,i) { return (legs[0][i].x1.animVal.value + legs[0][i].x2.animVal.value ) /2;})
+	.attr("y", function(d,i) { return (legs[0][i].y1.animVal.value + legs[0][i].y2.animVal.value ) /2;})
+	.attr("font-family", "sans-serif")
+        .attr("font-size", "12px")
+        .attr("fill", "black")
+	.attr("text-anchor", "middle")
+	.text( function(d){return d;});
+    
 };
 
 window.onload = function(){
+    updateMap();
     document.getElementsByTagName("body")[0].addEventListener('mouseup',updateMap);
 };
 
